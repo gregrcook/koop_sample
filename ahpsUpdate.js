bb03f04c84d1ba1e8ba33a4071988afb72b53fe2
Greg Cook <greg@gregrcook.com>
2015-07-27 03:15:13 +0000

    Updated test

--- ahpsUpdate.js
+++ ahpsUpdate.js
@@ -1,46 +1,69 @@
+var ogr2ogr = require('ogr2ogr');               // Require npm library definitions.
+var execSync = require('sync-exec');
+var fs = require('fs');
+
+var dateAndTime = getCurrentDateAndTime();
+
 function main() {
-  // This script queries an ArcServer installation for a kml file of river gauge data, converts the file to GeoJson and then updates the Koop configuration to serve the file.
-  // 
-  var ahpsKmlFile = getAhpsGaugeData(getCurrentDate());
-  console.log(ahpsKmlFile[0], ahpsKmlFile[1]);
-  var ahpsGeoJsonFile = convertToGeoJson(ahpsKmlFile[0], ahpsKmlFile[1], ahpsKmlFile[0].split(".kmz").join(".geojson"), ahpsKmlFile[1].split("kmz").join("geojson"));
-  console.log(ahpsGeoJsonFile[0], ahpsGeoJsonFile[1]);
+  // This script queries an ArcServer installation for an ESRI json file of forecasted river gauge data in Wisconsin, converts the file
+  // to GeoJson using ogr2ogr and then updates the Koop configuration to serve the file.
+
+  var url = buildArcServerQueryUrl(
+    "http://gis.srh.noaa.gov/arcgis/rest/services/ahps_gauges/MapServer/1/query?",
+    "json",
+    //"-180,-90,180,90",                          // World extent.
+    "-92.889433,42.49172,-86.249548,47.309822",   // Wisconsin extent.
+    "esriGeometryEnvelope",
+    "*",
+    "4326"
+  );
+  
+  var test = convertStreamToGeoJson(url, __dirname + "/geojson/", "ahps_gauge_data_" + dateAndTime, "gj");
+  //var test2 = updateToLatestFile(__dirname + "/geojson/", "ahps_gauge_data_" + dateAndTime + ".geojson");
+  //convertPathToGeoJson(__dirname + "/json/query.json", __dirname + "/geojson/", "query3", "gj");
 };
 
-function getAhpsGaugeData(date) {
-  // Generate and retrieve a kml file from the NOAA ArcServer. http://gis.srh.noaa.gov/arcgis/sdk/rest/index.html?generatekml.html
-  // Example kml REST request. http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StatesCitiesRivers_USA/MapServer/generatekml?docName=Test+Name&layers=1,2&layerOptions=separateImage
-  var url = "http://gis.srh.noaa.gov/arcgis/rest/services/ahps_gauges/MapServer/generateKml?docName=ahps_gauges_" + date + "&layers=0,1&layerOptions=composite"
-  var fileName = "ahps_gauges_" + date + ".kmz";
-  var fileLocation = __dirname + "/kmz/";
-  getFile(fileName, fileLocation, url)
-  return [fileName, fileLocation]
+function buildArcServerQueryUrl(baseUrl, f, geometry, geometryType, outfields, outSR) {
+  // http://gis.srh.noaa.gov/arcgis/sdk/rest/index.html?query.html
+  return baseUrl + "&f=" + f + "&geometry=" + geometry + "&geometryType=" + geometryType + "&outfields=" + outfields + "&outSR=" + outSR
 };
 
-function getFile(fileName, fileLocation, url) {
-  var curl = 'curl -o ' + fileLocation + fileName + ' ' + url;
-  var exec = require('sync-exec');
-
-  exec(curl, 100000);
-  console.log(fileName + ' downloaded to ' + fileLocation);
+function convertStreamToGeoJson(url, outputLocation, outputName, format) {
+  //var write_stream =fs.createWriteStream(outputLocation + outputName + returnExtension(format));
+  
+  ogr2ogr(url)
+    .format(returnFormat(format))
+    .skipfailures()
+    .stream()
+    .pipe(fs.createWriteStream(outputLocation + outputName + returnExtension(format)));
+    
+  ogr2ogr(url)        // Hackish until I figure out how to properly use streams.
+    .format(returnFormat(format))
+    .skipfailures()
+    .stream()
+    .pipe(fs.createWriteStream(outputLocation + "ahps_gauge_data_latest" + returnExtension(format)));
+    
+  /*write_stream.on('end', function() {
+    write_stream.end();
+    updateToLatestFile(__dirname + "/geojson/", "ahps_gauge_data_" + dateAndTime + ".geojson");
+  });*/
 };
 
-function convertToGeoJson(kmlFileName, kmlFileLocation, geoJsonFileName, geoJsonFileLocation) {
-  var tj = require('togeojson'),
-      fs = require('fs'),
-    // node doesn't have xml parsing or a dom. use jsdom
-    jsdom = require('node-jsdom').jsdom;
-    
-    var newestGeoJsonFileName = "ahps_gauges_newest.geojson"
-    var kml = jsdom(fs.readFileSync(kmlFileLocation + kmlFileName, 'utf8'));
-    var converted = JSON.stringify(tj.kml(kml));
-    
-    fs.writeFileSync(geoJsonFileLocation + geoJsonFileName, converted);
-    console.log(geoJsonFileName + ' writen to ' + geoJsonFileLocation);
-    
-    fs.writeFileSync(geoJsonFileLocation + newestGeoJsonFileName, converted);
-    console.log('Updated geoJson file created.')
-    return [geoJsonFileName, geoJsonFileLocation]
+function convertPathToGeoJson(fileLocation, outputLocation, outputName, format) {
+  ogr2ogr(fileLocation)
+    .format(returnFormat(format))
+    .destination(outputLocation + outputName + returnExtension(format))
+    //.options(['-simplify', '0.4'])
+    .exec(function (err, data) {
+      if (err) console.error(err);
+    });
+};
+
+function updateToLatestFile(fileLocation, fileName) {
+  fs.writeFileSync(__dirname + "/geojson/ahps_gauge_data_latest.geojson", JSON.parse(fileLocation + fileName), function (err) {
+  if (err) throw err;
+    console.log('It\'s saved!');
+  });
 };
 
 function updateConfigFile(fileName, fileLocation) {
@@ -48,23 +71,36 @@ function updateConfigFile(fileName, fileLocation) {
   
 };
 
-function getCurrentDate() {
+function returnFormat(format) {
+  switch (format) {
+    //case "shp": return "ESRI Shapefile"
+    case "gj": return "GeoJSON"
+    case "kml": return "KML"
+    default: return "GeoJSON"
+  }
+};
+
+function returnExtension(format) {
+  switch (format) {
+    //case "shp": return ".shp"
+    case "gj": return ".geojson"
+    case "kml": return ".kml"
+    default: return ".geojson"
+  }
+};
+
+function getCurrentDateAndTime() {
     // https://stackoverflow.com/questions/7357734/how-do-i-get-the-time-of-day-in-javascript-node-js
     var date = new Date();
     var year = date.getFullYear();
-
     var month = date.getMonth() + 1;
     month = (month < 10 ? "0" : "") + month;
-
     var day  = date.getDate();
     day = (day < 10 ? "0" : "") + day;
-    
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
-
     var min  = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
-    
     var sec  = date.getSeconds();
     sec = (sec < 10 ? "0" : "") + sec;
     return year + "_" + month + "_" + day + "_" + hour + ":" + min + ":" + sec;
